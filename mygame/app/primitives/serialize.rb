b949eb56b6d153d0f976ad58df9e3710aca5412c
Amir Rajan <ar@amirrajan.net>
2020-12-15 12:22:09 -0600

    Update serialize.rb

--- mygame/app/primitives/serialize.rb
+++ mygame/app/primitives/serialize.rb
@@ -1,1 +1,15 @@
-# todo
+module Serialize
+  def serialize
+    instance_variables.map do |key|
+      [key.to_s[1..-1].to_sym, instance_variable_get(key)]
+    end.to_h
+  end
+
+  def inspect
+    serialize.to_s
+  end
+
+  def to_s
+    serialize.to_s
+  end
+end
